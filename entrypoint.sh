ffe733ba41b53a742b0da8317ce34a9b7695f4f6
sajjadhossein <sajjadhosseinrezaei@yahoo.com>
2025-04-10 12:11:59 +0330

    remove user

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 python manage.py migrate
 
 # ساخت سوپریوزر فقط اگه وجود نداشته باشه
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('sajjadhossein', 'sajjadhossein@gmail.com', 'sajjad1379') if not User.objects.filter(username='sajjadhossein').exists() else print('Superuser already exists')" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser() if not User.objects.filter(username='sajjadhossein').exists() else print('Superuser already exists')" | python manage.py shell
 
 # اجرای سرور با exec
 exec gunicorn --bind 0.0.0.0:8000 mysite.wsgi:application
