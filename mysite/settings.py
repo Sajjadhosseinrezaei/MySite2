020c8128eada3d97e99f8a824e0608b42081597c
sajjadhossein <sajjadhosseinrezaei@yahoo.com>
2025-04-13 08:49:46 +0330

    decrease refresh token to 1 day

--- mysite/settings.py
+++ mysite/settings.py
@@ -212,5 +212,5 @@ from datetime import timedelta
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
